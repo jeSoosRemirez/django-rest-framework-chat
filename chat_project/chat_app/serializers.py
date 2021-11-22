e8aafcde37887091726ec223eeef1d0b3c687479
jeSoosRemirez <dkhomych0091@gmail.com>
2021-11-22 14:25:13 +0200

    Added few comments

--- chat_project/chat_app/serializers.py
+++ chat_project/chat_app/serializers.py
@@ -3,14 +3,18 @@ from chat_app.models import Message
 
 
 class MessageListSerializer(serializers.HyperlinkedModelSerializer):
-
+    """
+    Shows when we see a list of messages
+    """
     class Meta:
         model = Message
         fields = ['author', 'author_email', 'text', 'created_time']
 
 
 class MessageDetailSerializer(serializers.HyperlinkedModelSerializer):
-
+    """
+    Shows when we look at exact message
+    """
     class Meta:
         model = Message
         fields = ['id', 'author', 'author_email', 'text', 'created_time']
