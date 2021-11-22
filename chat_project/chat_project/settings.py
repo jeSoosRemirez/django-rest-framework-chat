1c7ff12f39dc33f900847eeb6a2067d03bea1045
jeSoosRemirez <dkhomych0091@gmail.com>
2021-11-22 14:47:42 +0200

    Added psql instead of sqlite in DATABASES

--- chat_project/chat_project/settings.py
+++ chat_project/chat_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'chat_app',
-    'restframework',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -78,8 +78,19 @@ WSGI_APPLICATION = 'chat_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'chat_messages_db',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'postgres',
+
+        'HOST': '127.0.0.1',    # Default LocalHost
+
+        'PORT': 5432,   # Default port
+
     }
 }
 
