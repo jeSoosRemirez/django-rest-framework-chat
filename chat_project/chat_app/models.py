c98e3cb24fd89e21694bf40b041826e77ce8181c
jeSoosRemirez <dkhomych0091@gmail.com>
2021-11-22 14:21:36 +0200

    Created models for message

--- chat_project/chat_app/models.py
+++ chat_project/chat_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    author = models.CharField(max_length=100, blank=True, default='Anon')
+    author_email = models.EmailField(max_length=254, blank=True, default='')
+    text = models.TextField(max_length=100, blank=False)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_time']
