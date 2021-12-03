71bb32822dcede499005bcf0ee2816bf08ab2e09
jeSoosRemirez <dkhomych0091@gmail.com>
2021-12-03 18:13:09 +0200

    Add SECURE_PROXY_SSL_HEADER

--- chat_project/chat_project/settings.py
+++ chat_project/chat_project/settings.py
@@ -147,3 +147,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
