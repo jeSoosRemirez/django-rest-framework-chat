8f67085bf32baee0c6d95438aff17b4c02d56cd5
jeSoosRemirez <dkhomych0091@gmail.com>
2021-12-05 22:19:34 +0200

    Add STATICFILES_DIRS

--- chat_project/chat_project/settings.py
+++ chat_project/chat_project/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'chat_project', 'static'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
